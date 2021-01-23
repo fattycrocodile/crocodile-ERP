22ce53cbeb4199218714a0d6814b0aa1d41042d2
fattycrocodile <fullbravery@gmail.com>
2021-01-23 22:33:03 +0600

    ROUTE PROBLEM FIXED

--- app/Modules/Config/routes/web.php
+++ app/Modules/Config/routes/web.php
@@ -1,3 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('config', 'ConfigController@welcome');
+
+
+Route::group(['middleware' => ['auth:web']], function () {
+    Route::get('/settings', 'SettingController@index')->name('admin.settings');
+    Route::post('/settings', 'SettingController@update')->name('admin.settings.update');
+
+});
