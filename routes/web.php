a6bc7ec1c6322813b4e4513af28c0451216b7b6d
fattycrocodile <fullbravery@gmail.com>
2021-01-23 22:01:39 +0600

    INVENTORY MODEL VIEW MOVE TO MODULES

--- routes/web.php
+++ routes/web.php
@@ -18,31 +18,6 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::group(['middleware' => ['auth:web']], function () {
-    Route::get('/dashboard', 'HomeController@index')->name('dashboard');
-
-    Route::get('/settings', 'SettingController@index')->name('admin.settings');
-    Route::post('/settings', 'SettingController@update')->name('admin.settings.update');
-
-    Route::group(['prefix' => 'config/categories'], function () {
-        Route::get('/', 'CategoryController@index')->name('admin.categories.index');
-        Route::get('/create', 'CategoryController@create')->name('admin.categories.create');
-        Route::post('/store', 'CategoryController@store')->name('admin.categories.store');
-        Route::get('/{id}/edit', 'CategoryController@edit')->name('admin.categories.edit');
-        Route::post('/update', 'CategoryController@update')->name('admin.categories.update');
-        Route::get('/{id}/delete', 'CategoryController@delete')->name('admin.categories.delete');
-    });
-
-    Route::group(['prefix' => 'config/brand'], function () {
-        Route::get('/', 'BrandController@index')->name('admin.brands.index');
-        Route::get('/create', 'BrandController@create')->name('admin.brands.create');
-        Route::post('/store', 'BrandController@store')->name('admin.brands.store');
-        Route::get('/{id}/edit', 'BrandController@edit')->name('admin.brands.edit');
-        Route::post('/update', 'BrandController@update')->name('admin.brands.update');
-        Route::get('/{id}/delete', 'BrandController@delete')->name('admin.brands.delete');
-    });
-
-});
 
 // it should be at the bottom of every routes
 Route::get('/{path}', function () {
