3eb2f3028b936103862335b0afc4b9d1d3a8b07f
fattycrocodile <fullbravery@gmail.com>
2021-02-04 00:57:17 +0600

    footer developer name aded

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,8 +1,9 @@
 <footer class="footer footer-static footer-light navbar-shadow">
     <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2">
       <span class="float-md-left d-block d-md-inline-block">
-          Copyright &copy; {{ now()->year }}
-          <a class="text-bold-800 grey darken-2" href="#" target="_blank">ERP </a>, All rights reserved.
+          Developer:
+          <a class="text-bold-800 grey darken-2" href="#" target="_blank">{{ config('settings.developed_by') }} </a>
       </span>
+      <span class="float-md-right d-block d-md-inline-block d-none d-lg-block">Made with <i class="ft-heart pink"></i></span>
     </p>
 </footer>
