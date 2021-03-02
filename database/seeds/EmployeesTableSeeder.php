46660e94f7429defa736f4ae7f5896f91e72a99b
fattycrocodile <fullbravery@gmail.com>
2021-03-02 22:49:16 +0600

    SEEDER ADDED FOR DEPARTMENT, DESIGNATION, EMPLOYEES

---
+++ database/seeds/EmployeesTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+use Illuminate\Database\Seeder;
+
+class EmployeesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory('App\Modules\Hr\Models\Employees', 10)->create();
+    }
+}
