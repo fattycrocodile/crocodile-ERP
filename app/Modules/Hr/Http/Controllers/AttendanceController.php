bd7ee7709124539c6658d200dfaf20f0eecd9e22
fattycrocodile <fullbravery@gmail.com>
2021-03-04 23:26:25 +0600

    attendance save

--- app/Modules/Hr/Http/Controllers/AttendanceController.php
+++ app/Modules/Hr/Http/Controllers/AttendanceController.php
@@ -11,9 +11,13 @@ use App\Modules\Hr\Models\Designations;
 use App\Modules\Hr\Models\Employees;
 use App\Traits\UploadAble;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Throwable;
+use Validator;
 
 class AttendanceController extends BaseController
 {
@@ -53,6 +57,52 @@ class AttendanceController extends BaseController
         return view('Hr::attendance.create', compact('departments', 'designations'));
     }
 
+    /**
+     * @param Request $request
+     * @return JsonResponse|RedirectResponse
+     * @throws Throwable|Throwable
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $this->validate($request, [
+            'date' => 'required|date',
+            'attendance' => 'required|array',
+        ]);
+        $params = $request->except('_token');
+        try {
+            $year = date('y');
+            $date = $params['date'];
+            $i = 0;
+            foreach ($params['attendance']['employee_id'] as $employee_id) {
+                $in_time = $params['attendance']['in_time'][$i];
+                $out_time = $params['attendance']['out_time'][$i];
+                $remarks = $params['attendance']['remarks'][$i];
+
+                if ($in_time != "") {
+//                    echo $in_time;
+                    $model = Attendance::where('employee_id', '=', $employee_id)->where('date', '=', $date)->first();
+                    if (!$model)
+                        $model = new Attendance();
+                    else
+                        $model->updated_by = auth()->user()->id;
+                    $model->employee_id = $employee_id;
+                    $model->date = $date;
+                    $model->in_time = $in_time;
+                    $model->out_time = $out_time;
+                    $model->comments = $remarks;
+                    $model->created_by = auth()->user()->id;
+                    $model->save();
+                }
+                $i++;
+            }
+            return $this->responseJson(false, 200, "Attendance Saved Successfully!");
+        } catch (QueryException $exception) {
+//            throw new InvalidArgumentException($exception->getMessage());
+            return $this->responseJson(true, 200, "Server problem!");
+        }
+        return $this->responseJson(true, 200, "Please try again!");
+//        return $this->responseJson(true, 200, "Something is wrong", $validator->errors()->all());
+    }
 
 
     public function getEmployeesListForAttendance(Request $request): ?JsonResponse
@@ -73,13 +123,18 @@ class AttendanceController extends BaseController
                 if ($designation_id > 0)
                     $data = $data->where('designation_id', '=', $designation_id);
             }
+            if ($request->has('employee_id')) {
+                $employee_id = trim($request->employee_id);
+                if ($employee_id > 0)
+                    $data = $data->where('id', '=', $employee_id);
+            }
             $data = $data->where('status', '=', Employees::ACTIVE);
             $data = $data->orderby('full_name', 'asc');
             $data = $data->get();
 
             $returnHTML = view('Hr::Attendance.employee-attendance-entry', compact('data', 'date'))->render();
             return $this->responseJson(false, 200, "DATA FOUND", $returnHTML);
-        } else{
+        } else {
             return $this->responseJson(true, 200, "DATA NOT FOUND");
         }
     }
